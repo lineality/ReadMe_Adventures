4006231c6626d299331d93115bafd3b68ad98261
Geoffrey Gordon Ashbrook <26224792+lineality@users.noreply.github.com>
2020-08-10 22:15:07 -0400

    Update scene_02.py

--- Learn_NLP__Dungeon_of_Scrolls/scene_02/scene_02.py
+++ Learn_NLP__Dungeon_of_Scrolls/scene_02/scene_02.py
@@ -6,7 +6,7 @@ import os  # for terminal functions etc.
 import random  # for randomized replies etc
 import sys  # for realistic printing
 import time  # for pauses
-import requests
+#import requests
 
 scene = "scene_02"
 adventure = "Learn_NLP__Dungeon_of_Scrolls"
