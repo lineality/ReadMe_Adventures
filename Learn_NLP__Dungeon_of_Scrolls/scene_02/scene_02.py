d661d2ffc6821aeecbbfdba494f0f8104ea061f9
Geoffrey Gordon Ashbrook <26224792+lineality@users.noreply.github.com>
2020-08-20 19:23:38 -0400

    Update scene_02.py

--- Learn_NLP__Dungeon_of_Scrolls/scene_02/scene_02.py
+++ Learn_NLP__Dungeon_of_Scrolls/scene_02/scene_02.py
@@ -1,4 +1,4 @@
-# version 015
+# version 016
 # Scene_02
 # Witch Map
 #  _____           _ _____
@@ -81,11 +81,37 @@ def slow_print(input_text):
     # end
     pass
 
+# making text print-out look like it is being typed
+def medium_print(input_text):
+    # staggered times
+    random_times = [0.08]
+    # stagger each character
+    for i in input_text:
+        time.sleep(random.choice(random_times))
+        print(i, end="")
+        # prints each time (before loop is finished)
+        sys.stdout.flush()
+    # end
+    pass
+
+# making text print-out look like it is being typed
+def fast_print(input_text):
+    # staggered times
+    random_times = [0.005]
+    # stagger each character
+    for i in input_text:
+        time.sleep(random.choice(random_times))
+        print(i, end="")
+        # prints each time (before loop is finished)
+        sys.stdout.flush()
+    # end
+    pass
+
 
 # making text print-out look like it is being typed
 def steady_print(input_text):
     # staggered times
-    wait_time = 0.013
+    wait_time = 0.015
     # stagger each character
     for i in input_text:
         time.sleep(wait_time)
@@ -134,7 +160,7 @@ def clear_terminal():
 #############
 
 # make sure file structure is correct
-manage_file_pathways()
+#manage_file_pathways()
 
 # general_wrong_answer_responces
 responses = [
@@ -208,13 +234,357 @@ elif pronoun == "they":
 
 # pronoun_phrase_1
 if pronoun == "he":
-    pronoun_phrase_1 = "he doesn't "
+    pronoun_phrase_1 = "he doesn't"
 
 elif pronoun == "she":
-    pronoun_phrase_1 = "she doesn't "
+    pronoun_phrase_1 = "she doesn't"
 
 elif pronoun == "they":
-    pronoun_phrase_1 = "they don't "
+    pronoun_phrase_1 = "they don't"
+
+
+
+#########################
+# Scene 2 Materials Files
+#########################
+
+# create map_1 file
+map_1 = """
+Welcome to the historic dungeons of
+Spring Falls Imperial Forest Reserves,
+home to the most highly reviewed dungeon restaurants
+and a family friendly enchanted forest retreat center.
+From the impasses of blister bay to the seasonal favorite
+'house of blood' that delight family travelers,
+you can also stop by our xylophone emporium for an acoustic mystery
+the family will never forget. (All instruments are made by humans
+on the premises accorded to Imperial law.)
+Fasting, loitering, and pets are prohibited.
+Directions: Head east past the Axis Mountains then follow
+the South Ridge mountains continuing south until you
+get to the third old growth pine forest
+and turn west at the base of the mountain.
+"""
+# create, write-to, & save .txt file
+file_to_create4 = open("map_1.txt", "w")
+file_to_create4.write(map_1)
+file_to_create4.close()
+
+
+# create map_2 file
+map_2 = """
+Here at the Murkland Designated Sites,
+we are proud to have received recognition
+for our 100% magic-free locally grown produce.
+Tours are temporarily behind schedule
+due to a wagon guild strike.
+Information about the fence around the park
+may be obtained with a freedom of information request
+submitted before the next full moon
+to the Bureau of Parks and Sacrifices. May the
+the great empire prevailed over threats of technology.
+Seasonal passes will resume availability
+during the livestock relation season.
+Due to a decrease in demand, trumpet filters will no longer be
+available even with a judicial order on park grounds. All hail the humans.
+Directions: Go to the base of Blossom Ridge Mountain.
+"""
+# create, write-to, & save .txt file
+file_to_create5 = open("map_2.txt", "w")
+file_to_create5.write(map_2)
+file_to_create5.close()
+
+
+
+
+##############
+# The Action!
+##############
+
+clear_terminal()
+
+pause_seconds(1)
+
+steady_print(
+    """
+
+     __             __   ___  __ 
+    |  \ |  | |\ | / _` |__  /  \ |\ |
+    |__/ \__/ | \| \__| |___ \__/ | \|
+     __   ___
+    /  \ |__ 
+    \__/ |
+     __   __   __   __             __
+    /__` /  ` |__) /  \ |    |    /__`
+    .__/ \__, |  \ \__/ |___ |___ .__/
+
+"""
+)
+
+
+
+
+medium_print("""
+    .....................................
+""")
+
+pause_seconds(1)
+
+clear_terminal()
+
+
+slow_print(scene + blurb)
+
+# Main Story
+typed_print(
+    """
+\n
+You walk to the back,
+carrying the hot pizza,
+over creaking wooden floor boards.
+\nThe rooms get quieter as you get further from the bar.
+You walk up to a round table in the back up against the wall
+with a small round dusty window overhead.\n
+Something splashes against the window.
+\n"Hey," someone at the table says as they look up.
+"Is that our pizza? I'm starving."\n\n
+"""
+)
+
+answer = input("Your Answer: Is this their pizza? (yes/no)\n")
+
+# clear screen (for windows or...everything else)
+os.system("cls" if os.name == "nt" else "clear")
+
+answer = answer.lower()
+if answer == "yes":
+    typed_print(
+        """
+    \n"Yayy!!!!"
+
+    You put the pizza on the table
+    and they start to grab slices and eat.
+    """
+    )
+else:
+    typed_print('\n"...but it smells so good..."\n')
+
+# Tip
+typed_print(
+    """
+"Ok, everyone, let's focus. What are we going to do
+about these maps?"
+
+"I think THAT one looks longer."
+
+"But it could be off by just one character.
+We need the one with the shorter text."
+
+"I know, I tried counting the letters last night but
+I kept coming up with slightly different numbers."
+
+"Can we just try both maps?"\n
+"""
+)
+
+# press
+input("\n  ...Press enter to continue...  \n")
+
+clear_terminal()
+
+typed_print(
+    """
+"They lead in opposite directions, it takes days to get there,
+one of them is probably a trap, and we have classes on Monday."
+
+"And I have to walk my Dog."
+
+"And Lavender has to walk her dog."
+
+"My Aunt's dog."
+
+"Her Aunt's dog. It's your Aunt's dog?
+Anyway, we shouldn't try them both.
+So we've got to buckle down and figure this out,
+no matter how long it takes us."\n
+"""
+)
+
+typed_print(f'"Hang on...HANG on...I BET {pronoun} can count...Can you count?"\n')
+
+
+typed_print("\nAll voices go silent as everyone turns their gaze to you.\n")
+
+answer_check = False
+# keep asking until answer is correct
+while answer_check is False:
+    answer = input('\n"...Can you count?" (yes/no)\n')
+    answer = answer.lower()
+    if answer == "yes":
+        answer_check = True
+    if answer_check is False:
+        typed_print(f'"{random.choice(responses)}"')
+
+# clear screen (for windows or...everything else)
+os.system("cls" if os.name == "nt" else "clear")
+
+typed_print(
+    """
+"You can? Oh my gosh, ok, which of these is longer?"
+
+"...I was thinking,
+they're almost more like pamphlets than maps, really."
+
+"We could call them...Folded-Publications?"
+
+"We got them from witches. Can we call witches 'publishers'?
+...I always wondered about that."
+
+"Whatever, map-pamphlets, leaflets,
+can you count how long these are? Like,
+how many characters are printed on each one?
+Like, letters and numbers and spaces and everything?"
+"""
+)
+
+# Press Enter to Continue
+input("\n  ...Press enter to continue...  \n")
+clear_terminal()
+
+# steady_print(
+#     """
+# \n\n(You can solve this in a Terminal! You can do it!)
+
+# 1. Open new Terminal in scene_02 folder (right click: 'open in terminal')
+#    (folders) ReadMe_Adventures ->  Learn_NLP__Dungeon_of_Scrolls ->
+#    -> scene_02 ->  
+
+# 2. Check you are in the correct directory with these commands:
+#     $ ls
+#     $ pwd
+
+# 3. Open+Read both map files, and check both of their lengths! Type:
+#    (Note: replace the "x" in example code below with real numbers)
+#      $ python3
+#    >>> mapx = open("map_x.txt", "r").read()
+#    >>> mapx = open("map_x.txt", "r").read()
+#    >>> print( len(mapx), len(mapx) ]
+
+# """
+# )
+
+steady_print(
+    """
+\n\n(You can solve this in a Terminal! You can do it!)
+
+Step 1. Open a new Terminal
+
+Step 2. Open & Read both map files, and check both of their lengths!
+        You want the "map_1.txt" and "map_2.txt" files.
+
+  Type: (not the '$' or '>' symbols)
+      $ python3
+    >>> mapx = open("map_x.txt", "r").read()
+    >>> mapx = open("map_x.txt", "r").read()
+    >>> print("map 1 is: ", len(mapx), "\nmap 2 is: ", len(mapx)) 
+
+"""
+)
+
+# lengths for answer-checking
+map1_file_to_read = open("map_1.txt", "r")
+map1_text = map1_file_to_read.read()
+map1_length = len(map1_text)
+
+map2_file_to_read = open("map_2.txt", "r")
+map2_text = map2_file_to_read.read()
+map2_length = len(map2_text)
+
+map_length_dict = {"map_1": map1_length, "map_2": map2_length}
+# typed_print(map1_length, map2_length)
+
+# Check the Player's Answer
+
+answer_check = False
+# keep asking until answer is correct
+while answer_check is False:
+    answer = input('\n"...How long is map one?"\n')
+    answer = float(answer)
+    answer = int(answer)
+    answer_check = answer == map_length_dict["map_1"]
+    if answer_check is False:
+        typed_print(f'"{random.choice(responses)}"')
+
+answer_check = False
+# keep asking until answer is correct
+while answer_check is False:
+    answer = input('\n"...Now, how about map two?"\n')
+    answer = float(answer)
+    answer = int(answer)
+    answer_check = answer == map_length_dict["map_2"]
+    if answer_check is False:
+        typed_print(f'"{random.choice(responses)}"')
+
+clear_terminal()
+
+typed_print('\n"That was awsome. Ok, quick vote: Should we bring the android?"\n')
+
+typed_print(
+    f'\n"You can\'t just bring {pronoun2} if {pronoun_phrase_1} want to come?"\n'
+)
+
+typed_print(f'\n"That\'s not what I meant but, whatever. Do we invite {pronoun2}?"\n')
+
+typed_print(f'\n"{pronoun.title()} can hear us...{pronoun4} standing right there."\n')
+
+typed_print(
+    """
+"Yes, awkward, sorry, bad planning here. But, what's the vote? Who says yes?"
+"""
+)
+
+time.sleep(2.5)
+
+typed_print(
+    """
+Everyone immidiately raises their hands.
+
+"Do you want to come find a dungeon?
+We'll tell you all about it on the way. Well, we don't know ALL about it,
+but, do you want to come?"
+"""
+)
+
+typed_print(f'\n"Oh, I hope {pronoun} says yes..."\n')
+
+answer_check = False
+
+while answer_check is False:
+    answer = input("\n(yes/no)")
+    answer = answer.lower()
+    # boolean check
+    answer_check = ("yes" in answer) or ("ok" in answer)
+    if answer_check is False:
+        typed_print(f'"{random.choice(responses)}"')
+
+if answer_check is True:
+
+    typed_print('\n"Yes!!!"')
+
+    slow_print("\n\nEnd of Scene 2\n\n")
+
+
+
+
+######################
+# For scenes 1,2 files temporarility put in Current Working Directory
+######################
+
+os.remove("map_1.txt")
+os.remove("map_2.txt")
+
+# make sure file structure is correct
+manage_file_pathways()
 
 ###############
 # Create Files
@@ -222,35 +592,27 @@ elif pronoun == "they":
 
 # create ReadMe.txt file
 readme_text = """
-ReadMe Scene__02 Witch Map
+ReadMe: Scene_02, Dungeon of Scrolls 
  _____           _ _____
 | __  |___ ___ _| |     |___
 |    -| -_| .'| . | | | | -_|
 |__|__|___|__,|___|_|_|_|___|
- _____   _             _
-|  _  |_| |_ _ ___ ___| |_ _ _ ___ ___ ___
-|     | . | | | -_|   |  _| | |  _| -_|_ -|
-|__|__|___|\_/|___|_|_|_| |___|_| |___|___|
+ _____   _               _
+|  _  |_| |_ _ ___ ___ _| |_ _ _ ___ ___ ___
+|     | . | | | -_|   |_   _| | |  _| -_|_ -|
+|__|__|___|\_/|___|_|_| |_| |___|_| |___|___|
 
-Instructions:
 
-1. Run The Game
-    1. Open Terminal
-    2. Get file      Command: wget https://raw.githubusercontent.com/lineality/ReadMe_Adventures/master/Learn_NLP__Dungeon_of_Scrolls/scene_02/scene_02.py
-    3. Run file      Command: python3 scene_02.py
+Instructions to Run Game:
 
-    ( For Windows_OS, see: https://docs.google.com/document/d/1p6R2LpBZtgs9IO349W1Zrx8u_Zy_5kgCxbJ64toOJXI/edit?usp=sharing )
+    Step 1. Open a Terminal
 
-2. Journal
-    Keep a Journal about what you are learning.
+    Step 2. Cut & Past one of these into Terminal
+          (macOS / Linux / etc ->) curl -O https://raw.githubusercontent.com/lineality/ReadMe_Adventures/master/Learn_NLP__Dungeon_of_Scrolls/scene_02/scene_02.py ; python3 scene_02.py 
+          (windows ->) curl -O scene_02.py https://raw.githubusercontent.com/lineality/ReadMe_Adventures/master/Learn_NLP__Dungeon_of_Scrolls/scene_02/scene_02.py ; python3 scene_02.py
 
-3. Lost or Curious
-    Check out the Content_Map.txt for this scene.
-    ( Content_Map.txt will be generated when .py is run )
-
-4. User Manual
-    Advanced Instructions: https://docs.google.com/document/d/1q2AiDPM0BpQal7ltm3sWk0suxLJSS7uX6S9yH44F_ZA/edit?usp=sharing
-
+    Step 3. Hit Enter
+          (No python? -> https://www.python.org/downloads/ )
 """
 
 # create, write-to, & save .txt file
@@ -542,233 +904,3 @@ file_to_create5.close()
 # map2_file_to_read = open("map_2.txt", "r")
 # map2_text = map2_file_to_read.read()
 # print(len(map2_text))
-
-
-##############
-# The Action!
-##############
-
-# clear screen (for windows or...everything else)
-os.system("cls" if os.name == "nt" else "clear")
-
-slow_print(scene + blurb)
-
-# Main Story
-typed_print(
-    """
-\n
-You walk to the back,
-carrying the hot pizza,
-over creaking wooden floor boards.
-\nThe rooms get quieter as you get further from the bar.
-You walk up to a round table in the back up against the wall
-with a small round dusty window overhead.\n
-Something splashes against the window.
-\n"Hey," someone at the table says as they look up.
-"Is that our pizza? I'm starving."\n\n
-"""
-)
-
-answer = input("Your Answer: Is this their pizza? (yes/no)\n")
-
-# clear screen (for windows or...everything else)
-os.system("cls" if os.name == "nt" else "clear")
-
-answer = answer.lower()
-if answer == "yes":
-    typed_print(
-        """
-    \n"Yayy!!!!"
-
-    You put the pizza on the table
-    and they start to grab slices and eat.
-    """
-    )
-else:
-    typed_print('\n"...but it smells so good..."\n')
-
-# Tip
-typed_print(
-    """
-"Ok, everyone, let's focus. What are we going to do
-about these maps?"
-
-"I think THAT one looks longer."
-
-"But it could be off by just one character.
-We need the one with the shorter text."
-
-"I know, I tried counting the letters last night but
-I kept coming up with slightly different numbers."
-
-"Can we just try both maps?"\n
-"""
-)
-
-# press
-input("\n  ...Press enter to continue...  \n")
-
-clear_terminal()
-
-typed_print(
-    """
-"They lead in opposite directions, it takes days to get there,
-one of them is probably a trap, and we have classes on Monday."
-
-"And I have to walk my Dog."
-
-"And Lavender has to walk her dog."
-
-"My Aunt's dog."
-
-"Her Aunt's dog. It's your Aunt's dog?
-Anyway, we shouldn't try them both.
-So we've got to buckle down and figure this out,
-no matter how long it takes us."\n
-"""
-)
-
-typed_print(f'"Hang on...HANG on...I BET {pronoun} can count...Can you count?"\n')
-
-
-typed_print("\nAll voices go silent as everyone turns their gaze to you.\n")
-
-answer_check = False
-# keep asking until answer is correct
-while answer_check is False:
-    answer = input('\n"...Can you count?" (yes/no)\n')
-    answer = answer.lower()
-    if answer == "yes":
-        answer_check = True
-    if answer_check is False:
-        typed_print(f'"{random.choice(responses)}"')
-
-# clear screen (for windows or...everything else)
-os.system("cls" if os.name == "nt" else "clear")
-
-typed_print(
-    """
-"You can? Oh my gosh, ok, which of these is longer?"
-
-"...I was thinking,
-they're almost more like pamphlets than maps, really."
-
-"We could call them...Folded-Publications?"
-
-"We got them from witches. Can we call witches 'publishers'?
-...I always wondered about that."
-
-"Whatever, map-pamphlets, leaflets,
-can you count how long these are? Like,
-how many characters are printed on each one?
-Like, letters and numbers and spaces and everything?"
-"""
-)
-
-# Press Enter to Continue
-input("\n  ...Press enter to continue...  \n")
-clear_terminal()
-
-steady_print(
-    """
-\n\n(You can solve this in a Terminal! You can do it!)
-
-1. Open new Terminal in scene_02 folder (right click: 'open in terminal')
-   (folders) ReadMe_Adventures ->  Learn_NLP__Dungeon_of_Scrolls ->
-   -> scene_02 ->  "map_1.txt" & "map_2.txt"
-
-2. Check you are in the correct directory with these commands:
-    $ ls
-    $ pwd
-
-3. Open+Read both map files, and check both of their lengths! Type:
-   (Note: replace the "x" in example code below with real numbers)
-     $ python3
-   >>> mapx = open("map_x.txt", "r").read()
-   >>> mapx = open("map_x.txt", "r").read()
-   >>> print( len(mapx), len(mapx) ]
-
-"""
-)
-
-# lengths for answer-checking
-map1_file_to_read = open("map_1.txt", "r")
-map1_text = map1_file_to_read.read()
-map1_length = len(map1_text)
-
-map2_file_to_read = open("map_2.txt", "r")
-map2_text = map2_file_to_read.read()
-map2_length = len(map2_text)
-
-map_length_dict = {"map_1": map1_length, "map_2": map2_length}
-# typed_print(map1_length, map2_length)
-
-# Check the Player's Answer
-
-answer_check = False
-# keep asking until answer is correct
-while answer_check is False:
-    answer = input('\n"...How long is map one?"\n')
-    answer = float(answer)
-    answer = int(answer)
-    answer_check = answer == map_length_dict["map_1"]
-    if answer_check is False:
-        typed_print(f'"{random.choice(responses)}"')
-
-answer_check = False
-# keep asking until answer is correct
-while answer_check is False:
-    answer = input('\n"...Now, how about map two?"\n')
-    answer = float(answer)
-    answer = int(answer)
-    answer_check = answer == map_length_dict["map_2"]
-    if answer_check is False:
-        typed_print(f'"{random.choice(responses)}"')
-
-clear_terminal()
-
-typed_print('\n"That was awsome. Ok, quick vote: Should we bring the android?"\n')
-
-typed_print(
-    f'\n"You can\'t just bring {pronoun2} if {pronoun_phrase_1} want to come?"\n'
-)
-
-typed_print(f'\n"That\'s not what I meant but, whatever. Do we invite {pronoun2}?"\n')
-
-typed_print(f'\n"{pronoun.title()} can hear us...{pronoun4} standing right there."\n')
-
-typed_print(
-    """
-"Yes, awkward, sorry, bad planning here. But, what's the vote? Who says yes?"
-"""
-)
-
-time.sleep(2.5)
-
-typed_print(
-    """
-Everyone immidiately raises their hands.
-
-"Do you want to come find a dungeon?
-We'll tell you all about it on the way. Well, we don't know ALL about it,
-but, do you want to come?"
-"""
-)
-
-typed_print(f'\n"Oh, I hope {pronoun} says yes..."')
-
-answer_check = False
-
-while answer_check is False:
-    answer = input("\n(yes/no)")
-    answer = answer.lower()
-    # boolean check
-    answer_check = ("yes" in answer) or ("ok" in answer)
-    if answer_check is False:
-        typed_print(f'"{random.choice(responses)}"')
-
-if answer_check is True:
-
-    typed_print('\n"Yes!!!"')
-
-    slow_print("\n\nEnd of Scene 2\n\n")
