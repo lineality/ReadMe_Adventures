b8468d0b4b77f61cb1686dd604a1f8934a5a9b32
Geoffrey Gordon Ashbrook <26224792+lineality@users.noreply.github.com>
2020-08-19 20:20:32 -0400

    Update scene_01.py

--- Learn_NLP__Dungeon_of_Scrolls/scene_01/scene_01.py
+++ Learn_NLP__Dungeon_of_Scrolls/scene_01/scene_01.py
@@ -80,6 +80,18 @@ def slow_print(input_text):
     # end
     pass
 
+# making text print-out look like it is being typed
+def medium_print(input_text):
+    # staggered times
+    random_times = [0.08]
+    # stagger each character
+    for i in input_text:
+        time.sleep(random.choice(random_times))
+        print(i, end="")
+        # prints each time (before loop is finished)
+        sys.stdout.flush()
+    # end
+    pass
 
 # making text print-out look like it is being typed
 def fast_print(input_text):
@@ -628,29 +640,32 @@ def correct_name(person, name):
 
 clear_terminal()
 
-fast_print(
+pause_seconds(1)
+
+steady_print(
     """
-  ______                                     
-  |  _  \                                    
-  | | | |_   _ _ __   __ _  ___  ___  _ __   
-  | | | | | | | '_ \ / _` |/ _ \/ _ \| '_ \  
-  | |/ /| |_| | | | | (_| |  __/ (_) | | | | 
-  |___/  \__,_|_| |_|\__, |\___|\___/|_| |_| 
-                      __/ |                  
-                     |___/                   
-          __   _____                _ _      
-         / _| /  ___|              | | |     
-    ___ | |_  \ `--.  ___ _ __ ___ | | |___  
-   / _ \|  _|  `--. \/ __| '__/ _ \| | / __| 
-  | (_) | |   /\__/ / (__| | | (_) | | \__ \ 
-   \___/|_|   \____/ \___|_|  \___/|_|_|___/
+
+     __             __   ___  __ 
+    |  \ |  | |\ | / _` |__  /  \ |\ |
+    |__/ \__/ | \| \__| |___ \__/ | \|
+     __   ___
+    /  \ |__ 
+    \__/ |
+     __   __   __   __             __
+    /__` /  ` |__) /  \ |    |    /__`
+    .__/ \__, |  \ \__/ |___ |___ .__/
 
 """
 )
 
-slow_print("...")
 
-pause_seconds(4)
+
+
+medium_print("""
+    .....................................
+""")
+
+pause_seconds(1)
 
 clear_terminal()
 
